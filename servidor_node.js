95178127911426c7fa28ffb37b3b8d7a7ba51730
nixeng2334 <dudinhabrrs61@gmail.com>
2026-08-06 11:44:23 -0300

    Add route to serve index.html

--- servidor_node.js
+++ servidor_node.js
@@ -18,6 +18,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 const ADMIN_USER = process.env.ADMIN_USER || "admin";
 
